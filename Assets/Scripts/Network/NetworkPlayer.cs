5b1f5209ee91fc1e8a18612e835984b08440dde5
Lior Ezra <lior.ez@gmail.com>
2015-09-01 23:24:59 +0300

    Camera network + score network

--- Assets/Scripts/Network/NetworkPlayer.cs
+++ Assets/Scripts/Network/NetworkPlayer.cs
@@ -1,12 +1,24 @@
 ﻿using UnityEngine;
 using System.Collections;
+using UnityStandardAssets.Vehicles.Car;
 
-public class NetworkPlayer : MonoBehaviour {
+public class NetworkPlayer : Photon.MonoBehaviour {
     public GameObject Camera;
-
 	// Use this for initialization
 	void Start () {
-	
+        if (photonView.isMine) //local car
+        {
+            Camera.SetActive(true);
+            //Camera.se
+            GetComponent<CarUserControl>().ControlEnabled = true;
+            GetComponentInChildren<Camera>().enabled = true;
+            GetComponent<Rigidbody>().useGravity = true;
+            GetComponent<PlayerController>().enabled = true;
+        }
+        else
+        {
+
+        }
 	}
 	
 	// Update is called once per frame
