eab4e70a30cd51bb241ae6d55217c990d49fd2c5
Lior Ezra <lior.ez@gmail.com>
2015-09-05 00:38:24 +0300

    Play collider

--- Assets/Scripts/Network/NetworkManager.cs
+++ Assets/Scripts/Network/NetworkManager.cs
@@ -36,17 +36,18 @@ public class NetworkManager : MonoBehaviour {
 //        GameObject player =  PhotonNetwork.Instantiate(prefabName, SpawnPoints[i].position, SpawnPoints[i].rotation, group);
         player = PhotonNetwork.Instantiate(prefabName, SpawnPoint1.position, SpawnPoint1.rotation, group);
         player.tag = Globals.Tags.Player.ToString();
-        player.transform.position = SpawnPoints[player.GetPhotonView().viewID/1000].position;
+        Debug.Log("prefab Name:" + prefabName + " number: " + player.GetPhotonView().viewID + "\nIsActive: " + player.GetActive());
+        
+        player.transform.position = SpawnPoints[(player.GetPhotonView().viewID/1000)%2].position;
         player.SetActive(true);
-        Debug.Log("prefab Name:" + prefabName + " number: " + player.GetPhotonView().viewID +"\nIsActive: " + player.GetActive());
         //create to spawn points 
     }
 
-    void OnLeftRoom()
-    {
-        //PhotonNetwork.
-        Debug.Log("Player #: " + player.GetPhotonView().viewID + " has left the room");
-    }
+    //void OnLeftRoom()
+    //{
+    //    //PhotonNetwork.
+    //    Debug.Log("Player #: " + player.GetPhotonView().viewID + " has left the room");
+    //}
 
     //private LoadBalancingClient client;
 
